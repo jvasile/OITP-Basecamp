f306c142a19a4f82367dc47de694df2a4b364436
James Vasile <james@jamesvasile.com>
2012-03-28 16:37:14 -0400

    Shavky updated the descrips

--- OITP_Basecamp.user.js
+++ OITP_Basecamp.user.js
@@ -9,4 +9,6 @@
 // ==/UserScript==
 
 var descrip = document.getElementById('time_entry_description');
-descrip.parentNode.innerHTML = '<select id="time_entry_description" name="time_entry[description]"><option value="Financially incubating open source software projects that build open internet technologies and circumvention tools.">Financially incubating projects</option><option value="Ensuring the ancillary development support that ultimately ensures the viability of any open source project.">Ancillary dev support</option><option value="Promoting project visibility and building a central web presence and developer portal.">OITP public outreach</option><option value="Hosting in-person developer meetings and code sprints.">Hackfests</option><option value="Hiring a core OITP staff to coordinate organizational set-up and assist on projects.">Hiring</option></select>';
+descrip.parentNode.innerHTML = '<select id="time_entry_description" name="time_entry[description]"><option>Incubating Open Source Projects</option><option>Hosting Code Sprints and Developer Convenings</option><option>Project Monitoring and Evaluation</option><option>In-kind Open Source Developer Time</option></select>';
+
+
